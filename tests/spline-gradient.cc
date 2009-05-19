2b640984a4406ecc6c44d91e39c61c3a9cc262dc
Thomas Moulard <thomas.moulard@gmail.com>
2009-05-19 17:55:17 +0900

    Enhance Spline gradient test case.
    	* tests/spline-gradient.cc: Display Spline for different
    	dummy parameters.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

--- tests/spline-gradient.cc
+++ tests/spline-gradient.cc
@@ -38,77 +38,90 @@ int run_test ()
 {
   Spline::vector_t params (4);
 
-  // Initial and final position (one dimensional spline).
-  params[0] = 0.,  params[1] = 0.33;
-  params[2] = 0.66,  params[3] = 1.;
-
-  Spline spline (std::make_pair (0., 1.), 1, params);
-  discreteInterval_t window (0., 1., 0.001);
-
-  std::cerr << spline << std::endl;
-
-  std::cout << "set term wxt persist font ',5'" << std::endl;
-  std::cout << "set multiplot layout 2,3" << std::endl;
-
-  for (unsigned i = 0; i < 3; ++i)
+  for (unsigned x = 0; x < 4; ++x)
     {
-      std::string title;
-      switch (i)
+      std::cout
+	<<
+	(boost::format
+	 ("set term wxt persist title 'Spline: base functions' %1% font ',5'")
+	 % x) << std::endl
+	<< "set grid xtics noytics linewidth 0.5" << std::endl
+	<< "set xlabel 'time'" << std::endl
+	<< "set ylabel 'spline value'" << std::endl
+	<< "set multiplot layout 5,3" << std::endl;
+
+
+
+      params.clear ();
+      params[x] = 1.;
+
+      Spline spline (std::make_pair (0., 4.), 1, params);
+      discreteInterval_t window (0., 4., 0.01);
+
+      std::cerr << spline << std::endl;
+
+      for (unsigned i = 0; i < 3; ++i)
 	{
-	case 0:
-	  title = "spline";
-	  break;
-	case 1:
-	  title = "spline derivative";
-	  break;
-	case 2:
-	  title = "spline 2nd derivative";
-	  break;
-	default:
-	  assert (0);
+	  std::string title;
+	  switch (i)
+	    {
+	    case 0:
+	      title = "spline";
+	      break;
+	    case 1:
+	      title = "spline derivative";
+	      break;
+	    case 2:
+	      title = "spline 2nd derivative";
+	      break;
+	    default:
+	      assert (0);
+	    }
+
+	  std::cout << "plot '-' title '"<< title <<"' with line\n" << std::endl;
+	  for (double t = boost::get<0> (window); t < boost::get<1> (window);
+	       t += boost::get<2> (window))
+	    {
+	      Spline::vector_t grad = spline.derivative (t, i);
+	      //std::cerr << jac.size1 () << "/" << jac.size2 () << std::endl;
+	      std::cout << (boost::format ("%1% %2%\n") % t % grad (0)).str ();
+	    }
+	  std::cout << "e" << std::endl;
 	}
 
-      std::cout << "plot '-' title '"<< title <<"' with line\n" << std::endl;
-      for (double t = boost::get<0> (window); t < boost::get<1> (window);
-	   t += boost::get<2> (window))
-	{
-	  Spline::vector_t grad = spline.derivative (t, i);
-	  //std::cerr << jac.size1 () << "/" << jac.size2 () << std::endl;
-	  std::cout << (boost::format ("%1% %2%\n") % t % grad (0)).str ();
-	}
-      std::cout << "e" << std::endl;
+      for (unsigned j = 0; j < 4; ++j)
+	for (unsigned i = 0; i < 3; ++i)
+	  {
+	    std::string title;
+	    switch (i)
+	      {
+	      case 0:
+		title = "configuration";
+		break;
+	      case 1:
+	    title = "derivative";
+	    break;
+	      case 2:
+		title = "2nd derivative";
+		break;
+	      default:
+		assert (0);
+	      }
+	    title.append ((boost::format (" (%1%)") % j).str ());
+
+	    std::cout << "plot '-' title '"<< title <<"' with line\n" << std::endl;
+	    for (double t = boost::get<0> (window); t < boost::get<1> (window);
+		 t += boost::get<2> (window))
+	      {
+		Spline::matrix_t jac = spline.variationDerivWrtParam (t, i);
+		//std::cerr << jac.size1 () << "/" << jac.size2 () << std::endl;
+		std::cout << (boost::format ("%1% %2%\n") % t % jac (0, j)).str ();
+	      }
+	std::cout << "e" << std::endl;
+	  }
+      std::cout << "unset multiplot" << std::endl;
     }
 
-  for (unsigned i = 0; i < 3; ++i)
-    {
-      std::string title;
-      switch (i)
-	{
-	case 0:
-	  title = "configuration variation";
-	  break;
-	case 1:
-	  title = "derivative variation";
-	  break;
-	case 2:
-	  title = "2nd derivative variation";
-	  break;
-	default:
-	  assert (0);
-	}
-
-      std::cout << "plot '-' title '"<< title <<"' with line\n" << std::endl;
-      for (double t = boost::get<0> (window); t < boost::get<1> (window);
-	   t += boost::get<2> (window))
-	{
-	  Spline::matrix_t jac = spline.variationDerivWrtParam (t, i);
-	  //std::cerr << jac.size1 () << "/" << jac.size2 () << std::endl;
-	  std::cout << (boost::format ("%1% %2%\n") % t % jac (0, 0)).str ();
-	}
-      std::cout << "e" << std::endl;
-    }
-
-  std::cout << "unset multiplot" << std::endl;
   return 0;
 }
 
