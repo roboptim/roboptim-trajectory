2fc502c861366f2cd71fd4e3a07e79922de3d192
Thomas Moulard <thomas.moulard@gmail.com>
2009-08-04 13:05:40 +0900

    Add orthogonal speed.
    	* include/Makefile.am: Distribute new headers.
    	* include/roboptim/trajectory/orthogonal-speed.hh: New.
    	* include/roboptim/trajectory/orthogonal-speed.hxx: New.
    
    Signed-off-by: Thomas Moulard <thomas.moulard@gmail.com>

---
+++ include/roboptim/trajectory/orthogonal-speed.hh
@@ -0,0 +1,55 @@
+// Copyright (C) 2009 by Thomas Moulard, AIST, CNRS, INRIA.
+//
+// This file is part of the roboptim.
+//
+// roboptim is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef ROBOPTIM_TRAJECTORY_ORTHOGONAL_SPEED_HH
+# define ROBOPTIM_TRAJECTORY_ORTHOGONAL_SPEED_HH
+# include <boost/shared_ptr.hpp>
+
+# include <roboptim/core/derivable-function.hh>
+# include <roboptim/trajectory/fwd.hh>
+# include <roboptim/trajectory/stable-time-point.hh>
+
+namespace roboptim
+{
+  template <typename T>
+  class OrthogonalSpeed : public DerivableFunction
+  {
+  public:
+
+    OrthogonalSpeed (StableTimePoint timePoint, const T& spline) throw ();
+    ~OrthogonalSpeed () throw ();
+
+    const T& trajectory () const throw ();
+
+    template <typename F, typename CLIST>
+    static void addToProblem (const T&,
+			      Problem<F, CLIST>&,
+			      typename Function::interval_t,
+			      unsigned);
+
+  protected:
+    void impl_compute (result_t& res, const argument_t& p) const throw ();
+    void impl_gradient (gradient_t& grad, const argument_t& p, size_type i)
+      const throw ();
+  private:
+    StableTimePoint timePoint_;
+    const T& trajectory_;
+  };
+} // end of namespace roboptim.
+
+# include <roboptim/trajectory/orthogonal-speed.hxx>
+#endif //! ROBOPTIM_TRAJECTORY_ORTHOGONAL_SPEED_HH
