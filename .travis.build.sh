e8a0fae6422185696902ece55caba497d751b938
Thomas Moulard <thomas.moulard@gmail.com>
2013-03-22 05:17:22 +0900

    Add Travis support.

---
+++ .travis.build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+# Directories.
+root_dir=`pwd`
+build_dir="$root_dir/_travis/build"
+install_dir="$root_dir/_travis/install"
+core_dir="$build_dir/roboptim-core"
+
+# Shortcuts.
+git_clone="git clone --quiet --recursive"
+
+# Create layout.
+rm -rf "$build_dir" "$install_dir"
+mkdir -p "$build_dir"
+mkdir -p "$install_dir"
+
+# Setup environment variables.
+export LD_LIBRARY_PATH="$install_dir/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$install_dir/lib/roboptim-core:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+# Checkout Ipopt.
+cd "$build_dir"
+wget "http://www.coin-or.org/download/source/Ipopt/Ipopt-3.10.3.tgz"
+tar xzvf Ipopt-3.10.3.tgz
+cd "$build_dir/Ipopt-3.10.3"
+cd ThirdParty/Mumps
+./get.Mumps
+cd "$build_dir/Ipopt-3.10.3"
+./configure --prefix="$install_dir"
+make
+make install
+
+# Checkout roboptim-core
+echo "Installing dependencies..."
+
+cd "$build_dir"
+$git_clone "git://github.com/roboptim/roboptim-core.git"
+cd "$core_dir"
+cmake . -DCMAKE_INSTALL_PREFIX:STRING="$install_dir"
+make install
+
+cd "$build_dir"
+$git_clone "git://github.com/roboptim/roboptim-core-plugin-ipopt.git"
+cd "$core_dir"
+cmake . -DCMAKE_INSTALL_PREFIX:STRING="$install_dir"
+make install
+
+# Build package
+echo "Building package..."
+cd "$build_dir"
+cmake "$root_dir" -DCMAKE_INSTALL_PREFIX="$install_dir"
+make
+make install
+#make test
