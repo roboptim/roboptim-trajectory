8bf2748845f7dc54b506cce86ecc9fc59bf7e78f
Thomas Moulard <thomas.moulard@gmail.com>
2013-03-27 11:18:20 +0900

    Install Eigen-3.1.2 in Travis build script.

--- .travis.build.sh
+++ .travis.build.sh
@@ -20,6 +20,18 @@ export LD_LIBRARY_PATH="$install_dir/lib:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$install_dir/lib/roboptim-core:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
 
+# Checkout Eigen.
+cd "$build_dir"
+wget "http://bitbucket.org/eigen/eigen/get/3.1.2.tar.gz"
+tar xzvf 3.1.2.tar.gz
+cd "$build_dir/eigen-eigen-5097c01bcdc4/"
+mkdir -p "$build_dir/eigen-eigen-5097c01bcdc4/_build"
+cd "$build_dir/eigen-eigen-5097c01bcdc4/_build"
+cmake .. -DCMAKE_INSTALL_PREFIX:STRING="$install_dir" \
+          -Dpkg_config_libdir:STRING="$install_dir/lib"
+make
+make install
+
 # Checkout Ipopt.
 cd "$build_dir"
 wget "http://www.coin-or.org/download/source/Ipopt/Ipopt-3.10.3.tgz"
